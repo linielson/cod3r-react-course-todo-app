be3b0444355b95088e289b7eae1e9dae1f3e6b49
Linielson Rosa <linielsonrosa@gmail.com>
2019-02-02 10:59:20 -0200

    Create PageHeader

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import PageHeader from '../template/pageHeader'
 
 export default (props) => (
   <div>
-    <h1>Sobre</h1>
+    <PageHeader name='Sobre' small='nós' />
+    <h3>Nossa História</h3>
+    <p>Lorem ipsum dolor sit amet...</p>
+    
+    <h3>Missão e Visão</h3>
+    <p>Lorem ipsum dolor sit amet...</p>
+    
+    <h3>Imprensa</h3>
+    <p>Lorem ipsum dolor sit amet...</p>
   </div>
 )

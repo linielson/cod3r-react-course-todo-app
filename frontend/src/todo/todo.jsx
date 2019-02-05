8cff678677ed9ef1300add1e4cd59a822930d907
Linielson Rosa <linielsonrosa@gmail.com>
2019-02-05 21:11:23 -0200

    Create events

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -4,11 +4,27 @@ import TodoForm from './todoForm'
 import TodoList from './todoList'
 
 export default class Todo extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { description: '', list: [] }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleAdd = this.handleAdd.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({ ...this.state,  description: event.target.value })
+  }
+
+  handleAdd() {
+    console.log(this)
+  }
+
   render() {
     return (
       <div>
         <PageHeader name='Tarefas' small='cadastro'/>
-        <TodoForm />
+        <TodoForm description={this.state.description} handleAdd={this.handleAdd} handleChange={this.handleChange}/>
         <TodoList />
       </div>
     )
